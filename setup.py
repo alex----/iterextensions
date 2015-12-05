f2bc01002145f010a7377a077e830a8ce256bce2
alex---- <alex----@users.noreply.github.com>
2015-12-04 21:18:11 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 
 
 setup(name='iterextensions',
-      version='0.0.1',
-      description='itertool helps for Python2/3',
+      version='0.0.2',
+      description='itertool helpers for Python2/3',
       author='Alex',
       url='https://github.com/alex----/iterextensions',
       packages=['iterextensions'])
